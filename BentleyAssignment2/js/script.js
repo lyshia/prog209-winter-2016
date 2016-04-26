303f4c329e1762ba48b7ba1b07865dc6d168002c
Alyshia Bentley <alyshiabentley@gmail.com>
2016-04-26 14:48:49 -0700

    beautify

--- BentleyAssignment2/js/script.js
+++ BentleyAssignment2/js/script.js
@@ -7,51 +7,42 @@ var select = document.getElementById("selection");
 var errorMess = document.getElementById("error");
 var hex = document.getElementById("hexCode");
 
- //functions
-function colorChooser(e)
-{
+//functions
+function colorChooser(e) {
     //decaled values down here instead of above so I do not have to call them twice, and they change each time i click
     var r = parseInt(document.getElementById("red").value);
     var g = parseInt(document.getElementById("green").value);
     var b = parseInt(document.getElementById("blue").value);
- 
 
-        if (validate(r, g, b)) {
-            select.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
-            errorMess.style.visibility = "hidden";
-            hex.style.visibility = "visible";
-            hex.innerHTML = "Hex code: #" + toHex(r, g, b).toUpperCase();
+    if (validate(r, g, b)) {
+        select.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+        errorMess.style.visibility = "hidden";
+        hex.style.visibility = "visible";
+        hex.innerHTML = "Hex code: #" + toHex(r, g, b).toUpperCase();
 
-        }
-        else {
-            errorMess.style.visibility = "visible";
-            hex.style.visibility = "hidden";
-            errorMess.innerHTML = "Please select a valid number";
-        }
-
+    } else {
+        errorMess.style.visibility = "visible";
+        hex.style.visibility = "hidden";
+        errorMess.innerHTML = "Please select a valid number";
     }
 
+}
+
 // convert to hexadecimal
-function toHex(r,g,b)
-{
+function toHex(r, g, b) {
     //return each value as a string, since hexadecimal is a string.
     // using a radix of 16, will return the hexadecimal value
     return r.toString(16) +
-          g.toString(16) +
-          b.toString(16);    
+        g.toString(16) +
+        b.toString(16);
 }
 
-
 //validate input
-function validate(r,g,b)
-{
+function validate(r, g, b) {
     //if out of range
-  if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 || isNaN(r) || isNaN(g)|| isNaN(b))
-    {
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 || isNaN(r) || isNaN(g) || isNaN(b)) {
         return false;
-    }
-    else
-    {
+    } else {
         return true;
     }
 }
